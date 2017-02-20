801742cf2dff7997381e40b3e7515c24fe92dcb9
Ryan Rosol <rosolrya@cs.grinnell.edu>
2017-02-20 14:00:19 -0600

    Reading Exercise 2-20

---
+++ 2-20/LinkedList.java
@@ -0,0 +1,9 @@
+
+public class LinkedList {
+	Node first;
+	
+	public void list_t(Node first) {
+		this.first = first;
+	}
+
+}
---
+++ 2-20/Node.java
@@ -0,0 +1,10 @@
+
+public class Node {
+	public int value;
+	Node next;
+	
+	public  void node_t(int value, Node next) {
+		this.value = value;
+		this.next = next;
+	}
+}
